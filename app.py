348de12a8d4cd4ac9c70acf078aa1ee2b60594b7
ckdeg <ckdeguz@gmail.com>
2025-11-08 21:36:09 -0500

    connected db and added /register

--- app.py
+++ app.py
@@ -1,9 +1,45 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import mysql.connector
 
 #sets up flask app and allows frontend to communicate with backend
 app = Flask(__name__)
 CORS(app)
+
+db_config = {
+  'host': 'localhost',
+  'user': 'root',
+  'password': '9pJJBAsql',
+  'database': 'cs336project'
+}
+
+def get_db_connection():
+  return mysql.connector.connect(**db_config)
+
+
+@app.route('/')
+def home():
+  return 'testing'
+
+
+#registering new user
+@app.route('/register', methods=['POST'])
+def register():
+  data = request.json
+  username = data.get("username")
+  password = data.get("password")
+
+  conn = get_db_connection()
+  cursor = conn.cursor()
+
+  cursor.execute("INSERT INTO user (username, password) VALUES (%s, %s)", (username, password))
+  conn.commit()
+
+  cursor.close()
+  conn.close()
+
+  return jsonify({"message": "User registered."})
+
 
 #people who submit a username and password will have their
 #credentials validated 
