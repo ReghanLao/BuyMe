c294845cd3f74c658382c7bf52d68fabaae25116
ckdeg <ckdeguz@gmail.com>
2025-11-09 22:20:58 -0500

    backend and config.py for DB variables

---
+++ backend/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import mysql.connector
+from config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
+
+#sets up flask app and allows frontend to communicate with backend
+app = Flask(__name__)
+CORS(app)
+
+db_config = {
+  'host': DB_HOST,
+  'user': DB_USER,
+  'password': DB_PASSWORD,
+  'database': DB_NAME
+}
+
+def get_db_connection():
+  return mysql.connector.connect(**db_config)
+
+#people who submit a username and password will have their
+#credentials validated 
+@app.route('/login', methods=['POST'])
+def login():
+  #retrieve data submitted from frontend
+  data = request.json 
+  username = data.get("username")
+  password = data.get("password")
+
+  conn = get_db_connection()
+  cursor = conn.cursor(dictionary=True)
+
+  cursor.execute("SELECT * FROM user WHERE username = %s AND password = %s", (username, password))
+  user = cursor.fetchone()
+
+  cursor.close()
+  conn.close()
+
+  #TO DO (once christian set up database)
+  #verify username exists in db and ensure credentials are valid
+  #return json back to frontend saying whether the user is logged in or not
+  if user:
+    return jsonify({"message": f"Hello, {user['username']}."})
+  else:
+    return jsonify({"message": "Invalid username or password"})
+
+if __name__ == "__main__":
+  app.run()
---
+++ backend/config.py
@@ -0,0 +1,5 @@
+# change to your information
+DB_HOST = "localhost"
+DB_USER = "root"
+DB_PASSWORD = "9pJJBAsql"
+DB_NAME = "cs336project"
