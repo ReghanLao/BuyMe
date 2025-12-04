d39841e9b4342428e5a882e21874c0a407a1f58c
1337haxorguy <1337haxorguy1@gmail.com>
2025-12-03 20:48:36 -0500

    auctions and bids tables

--- backend/queries.sql
+++ backend/queries.sql
@@ -37,3 +37,38 @@ CREATE TABLE pants (
   brand       VARCHAR(100),
   FOREIGN KEY (iid) REFERENCES item(iid)
 );
+
+CREATE TABLE auction (
+  auction_id      INT AUTO_INCREMENT PRIMARY KEY,
+
+  item_id         INT NOT NULL,
+  seller_id       INT NOT NULL,
+
+  initial_price   DECIMAL(10,2) NOT NULL,
+  min_sell_price  DECIMAL(10,2) NOT NULL,
+  bid_increment   DECIMAL(10,2) NOT NULL,
+
+  start_time      DATETIME NOT NULL,
+  end_time        DATETIME NOT NULL,
+
+  status          ENUM('scheduled','running','closed') NOT NULL DEFAULT 'scheduled',
+
+  current_price   DECIMAL(10,2) NOT NULL,
+  winner_id       INT NULL,
+
+  FOREIGN KEY (item_id) REFERENCES item(iid),
+
+  FOREIGN KEY (seller_id) REFERENCES user(uid),
+
+  FOREIGN KEY (winner_id) REFERENCES user(uid),
+);
+
+CREATE TABLE bid (
+  bid_id       INT AUTO_INCREMENT PRIMARY KEY,
+  auction_id   INT NOT NULL,
+  bidder_id    INT NOT NULL,
+  amount       DECIMAL(10,2) NOT NULL,
+  created_at   DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (auction_id) REFERENCES auction(auction_id),
+  FOREIGN KEY (bidder_id)  REFERENCES user(uid),
+);
